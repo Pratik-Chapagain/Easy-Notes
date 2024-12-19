140a739467a971731814570cc04a88404ca58b9a
Pratik Chapagain <chapagainpratik28@gmail.com>
2024-12-19 17:00:30 +0545

    push

--- easy-notes/src/components/Hero.jsx
+++ easy-notes/src/components/Hero.jsx
@@ -1,10 +1,48 @@
-const Hero = () => {
-    return (
-      <div>
-        <h1>Welcome to the Hero Section</h1>
+import React from "react";
+
+function Hero() {
+  return (
+    <section
+      style={{
+        position: 'relative', // Positioning context for absolute elements
+        padding: '50px 20px',
+        textAlign: 'center',
+        color: '#fff',
+      }}
+    >
+      {/* Rectangle 1 */}
+      <div
+        style={{
+          boxSizing: 'border-box',
+          position: 'absolute',
+          width: '670px',
+          height: '517px',
+          left: '387px',
+          top: '170px',
+          background: '#7C89CA',
+          border: '1px solid #D9D9D9',
+        }}
+      ></div>
+
+      {/* Add Book */}
+      <div
+        style={{
+          position: 'absolute',
+          width: '464px',
+          height: '253px',
+          left: '488px',
+          top: '223px',
+          background: 'url("/path-to-image.png") no-repeat center center',
+          backgroundSize: 'cover',
+        }}
+      ></div>
+
+      {/* Hero Icon */}
+      <div style={{ position: 'relative', zIndex: 1 }}>
+        
       </div>
-    );
-  };
-  
-  export default Hero;
-  
+    </section>
+  );
+}
+
+export default Hero;
